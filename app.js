b1cf1c22b3be4e9f96325375f77f8e86430dcd53
Abby <66693106+Abiollagh@users.noreply.github.com>
2022-09-09 06:19:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-// const mongoose = require("mongoose");
-// const encrypt = require("mongoose-encryption");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 console.log(process.env.API_KEY);
@@ -16,17 +16,17 @@ app.use(bodyParser.urlencoded({
 }));
 
 
-// mongoose.connect("mongodb://localhost: 27017/userDB", {
-//   useNewUrlParser: true
-// });
-//
-// const userSchema = new mongoose.Schema({
-//   email: String,
-//   password: String
-// });
-// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
-//
-// const User = new mongoose.model("User", userSchema);
+mongoose.connect("mongodb://localhost: 27017/userDB", {
+  useNewUrlParser: true
+});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
+
+const User = new mongoose.model("User", userSchema);
 
 
 
